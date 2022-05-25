de85a5b8faf9e9b84e367ed2d68ec63cfeeff7b1
cataldoivan <cmiarg@gmail.com>
2022-05-25 02:22:30 -0300

    se agrega mapa, y distintas formas de hacer fetch en API

--- weather-api/src/App.js
+++ weather-api/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Form from "./Form/Form";
 import MainInfo from "./MainInfo/MainInfo";
+import Axios from "axios"
 
 function App() {
 const[data,setData]=useState(null)
@@ -12,7 +13,19 @@ useEffect(()=>{
 },[])
 
 const loadInfo =async  (city="buenos aires")=>{
-  try {
+
+ fetch(`http://api.weatherapi.com/v1/current.json?key=fde928b461004847a7121945222405&q=${city}&aqi=no`)
+.then((data)=>data.json())
+.then((res)=>setData(res))
+console.log(data);
+  /* 
+Axios.get(`http://api.weatherapi.com/v1/current.json?key=fde928b461004847a7121945222405&q=${city}&aqi=no`)
+.then(Response=>{
+  setData(Response.data)
+  console.log(Response.data)
+})
+ */
+  /* try {
     const request=await fetch(`http://api.weatherapi.com/v1/current.json?key=fde928b461004847a7121945222405&q=${city}&aqi=no`)
     const requesyJson=await request.json()
     setData(requesyJson)
@@ -24,7 +37,8 @@ const loadInfo =async  (city="buenos aires")=>{
     console.log(city);
     
   }
- 
+  */
+
 }
 const handleChangeCity=(city)=>{
   loadInfo(city)
@@ -39,9 +53,17 @@ const handleChangeCity=(city)=>{
       <Form onChangeCity={handleChangeCity}/>
       </div>
       <div>
+      {data? 
         <MainInfo weather={data}/>
+      :null}
       </div>
+     
+        {data?
+      <iframe className="iframe" src={`https://maps.google.com/?ll=${data.location.lat},${data.location.lon}7&z=10&t=m&output=embed`} height="600" width="800" style={{border:0}} allowfullscreen  aria-hidden="false" tabindex="0"></iframe>
+         :null}
+      
     </div>
+
   );
 }
 
